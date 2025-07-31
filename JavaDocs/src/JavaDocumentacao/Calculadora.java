e68c0b5f1d236e8d531ed4c376191b216cab9153
Hudson Sena <hudsonsena93@gmail.com>
2025-07-31 17:14:59 -0300

    Update new project JavaDocs comments

--- JavaDocs/src/JavaDocumentacao/Calculadora.java
+++ JavaDocs/src/JavaDocumentacao/Calculadora.java
@@ -1,11 +1,17 @@
 package JavaDocumentacao;
-
+/**
+ * Calculadora Simples
+ * @author Hudson Diego
+ * @since v1.0
+ */
 public class Calculadora {
-    /*
+    /**
     * Realiza a divisão entre dois números inteiros
     * @param dividendo Número que  será dividido
     * @param divisor   Némero que irá dividir o dividendo
-    * @return
+    * @return Resultado da divisao entre o divisor e dividendo
+    * @author Hudson Diego
+    * @exception java.lang.ArithmeticException em caso de passar o divisor igual a zero
     */
     Integer dividir(Integer dividendo, Integer divisor){
         return dividendo/divisor;
