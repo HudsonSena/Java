e814b12ba3dc1808278639f15da8077f99a81664
Hudson Sena <hudsonsena93@gmail.com>
2025-08-06 17:05:36 -0300

    Update new project controle_de_fluxo_condicional

---
+++ JavaDocs/src/JavaControledeFluxo/condicional/CaixaEletronico.java
@@ -0,0 +1,17 @@
+package JavaControledeFluxo.condicional;
+
+public class CaixaEletronico {
+    public static void main(String[] args) {
+        double saldo = 25.0;
+        double valorSolicitado = 30.0;
+
+        if(valorSolicitado <= saldo) {
+            saldo = saldo - valorSolicitado;
+            System.out.println("Saque realizado com sucesso!");
+        } else
+            System.out.println("Saldo insuficiente!");
+        System.out.println("Saldo: " + saldo);
+        System.out.println("Valor solicitado: " + valorSolicitado);
+
+    }
+}
