2ef2b849941904ea4579b6c9b0f1105e8ef3cfc5
Hudson Sena <hudsonsena93@gmail.com>
2025-08-13 16:54:27 -0300

    Update new project exceptions finalized

--- JavaDocs/src/JAvaExcecoesP01/Excecao001.java
+++ JavaDocs/src/JAvaExcecoesP01/Excecao001.java
@@ -1,34 +1,52 @@
 package JAvaExcecoesP01;
 
+import java.util.InputMismatchException;
 import java.util.Locale;
 import java.util.Scanner;
 
 public class Excecao001 {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+        try {
+            Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
 
-        System.out.println("Digite seu nome: ");
-        String nome = scanner.next();
-        //Caso não digite nada
+            System.out.println("Digite seu nome: ");
+            String nome = scanner.next();
+            //Caso não digite nada
 
-        System.out.println("Digite seu sobrenome: ");
-        String sobrenome = scanner.next();
-        //Caso não digite nad
+            System.out.println("Digite seu sobrenome: ");
+            String sobrenome = scanner.next();
+            //Caso não digite nad
 
-        System.out.println("Digite sua idade: ");
-        int idade = scanner.nextInt();
-        //Valor da idade não numérico
-        //java.util.InputMismatchException
+            System.out.println("Digite sua idade: ");
+            int idade = scanner.nextInt();
+            //Valor da idade não numérico
+            //java.util.InputMismatchException
 
-        System.out.println("Digite sua altura: ");
-        double altura = scanner.nextDouble();
-        //Valor possuir virgula, ao invés de ponto
-        //java.util.InputMismatchException
+            System.out.println("Digite sua altura: ");
+            double altura = scanner.nextDouble();
 
-        //imprimindo os dados obtidos pelo usuario
-        System.out.println("Olá, me chamo " + nome.toUpperCase() + " " + sobrenome.toUpperCase());
-        System.out.println("Tenho " + idade + " anos ");
-        System.out.println("Minha altura é " + altura + " metros");
-        scanner.close();
+            //try cath em volta de um código/função específica
+            //double altura = 0;
+            /*try {
+                altura = scanner.nextDouble();
+            } catch (InputMismatchException e) {
+                System.err.println("A alura deve ser digitada no padrão americano: 0.00");
+            }*/
+            //Valor possuir virgula, ao invés de ponto
+            //java.util.InputMismatchException
+
+            //imprimindo os dados obtidos pelo usuario
+            System.out.println("Olá, me chamo " + nome.toUpperCase() + " " + sobrenome.toUpperCase());
+            System.out.println("Tenho " + idade + " anos ");
+            System.out.println("Minha altura é " + altura + " metros");
+            scanner.close();
+        } //Try Cath generalizado em volta do código completo.
+        catch (InputMismatchException e){
+            System.err.println("Digite os números na forma correta. Idade: 20. Altura: 0.00 no padrão americano");
+        } catch (NullPointerException e){
+            System.err.println("Não deixe o nome ou sobrenome em branco");
+        } catch (Exception ex){
+            ex.printStackTrace();
+        }
     }
 }
--- JavaDocs/src/JAvaExcecoesP01/Excecao002.java
+++ JavaDocs/src/JAvaExcecoesP01/Excecao002.java
@@ -1,13 +1,25 @@
 package JAvaExcecoesP01;
 
+import java.text.DecimalFormat;
+import java.text.DecimalFormatSymbols;
 import java.text.NumberFormat;
+import java.text.ParseException;
 
 public class Excecao002 {
     public static void main(String[] args) {
-        Number valor = Double.valueOf("a1.75");
-
-        valor = NumberFormat.getInstance().parse("a1.75");
-
-        System.out.println(valor);
+        try{
+            //Double valor = Double.valueOf("a1.75");
+            DecimalFormat df = new DecimalFormat();
+            DecimalFormatSymbols symbols = new DecimalFormatSymbols();
+            symbols.setDecimalSeparator(',');
+            symbols.setGroupingSeparator('.');
+            df.setDecimalFormatSymbols(symbols);
+            Number num = df.parse("1,75");
+            System.out.println(num.doubleValue());
+            //Number valor = NumberFormat.getInstance().parse("a1.75");
+        } catch (ParseException e){
+            //e.printStackTrace();
+            System.err.println("Número Inválido");
+        }
     }
 }
